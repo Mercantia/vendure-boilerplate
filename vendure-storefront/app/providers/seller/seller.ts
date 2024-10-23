e28963bbc1a49d4849b9bfbe1fe48896506595ec
Antonio Müller <mullerjk@gmail.com>
2024-10-23 06:58:54 -0300

    fixing crash

--- vendure-storefront/app/providers/seller/seller.ts
+++ vendure-storefront/app/providers/seller/seller.ts
@@ -2,56 +2,20 @@ import gql from "graphql-tag";
 import { MutationRegisterNewSellerArgs } from "~/generated/graphql";
 import { QueryOptions, sdk } from "~/graphqlWrapper";
 
-// Função para registrar novo vendedor
 export const registerSellerAccount = async (
-  options: QueryOptions,
-  variables: MutationRegisterNewSellerArgs
-) => {
-  return sdk.registerNewSeller(variables, options);
-};
+    options: QueryOptions,
+    variables: MutationRegisterNewSellerArgs,
+  ) => {
+    return sdk.registerNewSeller(variables, options);
+  };
 
-// Query para buscar os campos customizados do vendedor
-gql`
-  query GetSeller($id: ID!) {
-    seller(id: $id) {
+  gql`
+  mutation registerNewSeller($input: RegisterSellerInput!) {
+    registerNewSeller(input: $input) {
+      __typename
       id
-      name
-      customFields {
-        cnpj
-        companyName
-        tradingName
-        stateRegistration
-        municipalRegistration
-        businessPhone
-        responsiblePerson
-      }
+      code
+      token
     }
   }
 `;
-
-// Função para buscar os dados do vendedor
-export const getSellerData = async (sellerId: string) => {
-  const response = await sdk.getSeller({ id: sellerId });
-  return response.seller;
-};
-
-// Query para buscar as coleções do vendedor
-gql`
-  query GetCollectionsForSeller($sellerId: ID!) {
-    collections(sellerId: $sellerId) {
-      items {
-        id
-        name
-        featuredAsset {
-          preview
-        }
-      }
-    }
-  }
-`;
-
-// Função para buscar as coleções do vendedor
-export const getCollectionsForSeller = async (sellerId: string) => {
-  const response = await sdk.getCollectionsForSeller({ sellerId });
-  return response.collections.items;
-};
--- vendure-storefront/app/utils/vendor-url.ts
+++
@@ -1,8 +0,0 @@
-import { useLocation } from "@remix-run/react";
-
-const getVendorNameFromUrl = () => {
-  const location = useLocation();
-  const pathSegments = location.pathname.split('/');
-  const vendorName = pathSegments[1]; // Vendor name from the URL
-  return vendorName;
-};
