2bd0047c9d21c258c0eafc4bd9f77211a30ceb18
Antonio Müller <mullerjk@gmail.com>
2024-10-23 23:24:25 -0300

    index

--- vendure-storefront/app/routes/index.tsx
+++ vendure-storefront/app/routes/index.tsx
@@ -1,21 +1,22 @@
-import { useLoaderData } from '@remix-run/react';
-import { getCollections } from '~/providers/collections/collections';
-import { CollectionCard } from '~/components/collections/CollectionCard';
+// import { useLoaderData } from '@remix-run/react';
+// import { getCollections } from '~/providers/collections/collections';
+// import { CollectionCard } from '~/components/collections/CollectionCard';
 import { BookOpenIcon } from '@heroicons/react/24/solid';
 import { LoaderArgs } from '@remix-run/server-runtime';
 import { useTranslation } from 'react-i18next';
 
 export async function loader({ request }: LoaderArgs) {
-  const collections = await getCollections(request, { take: 20 });
+  //const collections = await getCollections(request, { take: 20 });
   return {
-    collections,
+    //collections,
   };
 }
 
 export default function Index() {
-  const { collections } = useLoaderData<typeof loader>();
+  //const { collections } = useLoaderData<typeof loader>();
   const { t } = useTranslation();
-  const headerImage = collections[0]?.featuredAsset?.preview;
+  //const headerImage = collections[0]?.featuredAsset?.preview;
+  const headerImage = null;
 
   return (
     <>
@@ -45,17 +46,17 @@ export default function Index() {
           <p className="mt-4 text-2xl text-white">
             {t('vendure.intro')}{' '}
             <a
-              href="https://www.vendure.io"
+              href="https://mercantia.app"
               className="text-blue-300 hover:text-blue-500"
             >
-              Vendure
+              Mercantia
             </a>{' '}
             &{' '}
             <a
               href="~/routes/__cart/index"
               className="text-red-300 hover:text-red-500"
             >
-              Remix
+              Kelebra
             </a>
           </p>
           <p className="mt-4 text-gray-300 space-x-1">
@@ -63,7 +64,7 @@ export default function Index() {
             <span>{t('common.readMore')}</span>
             <a
               className="text-primary-200 hover:text-primary-400"
-              href="https://www.vendure.io/blog/2022/05/lightning-fast-headless-commerce-with-vendure-and-remix"
+              href="https://mercantia.app"
             >
               {t('vendure.link')}
             </a>
@@ -71,41 +72,41 @@ export default function Index() {
         </div>
       </div>
 
-      <section
+      {/* <section
         aria-labelledby="category-heading"
         className="pt-24 sm:pt-32 xl:max-w-7xl xl:mx-auto xl:px-8"
-      >
+      > 
         <div className="px-4 sm:px-6 lg:px-8 xl:px-0">
           <h2
             id="category-heading"
             className="text-2xl font-light tracking-tight text-gray-900"
           >
-            {t('common.shopByCategory')}
+            {/* {t('common.shopByCategory')} 
           </h2>
         </div>
 
-        <div className="mt-4 flow-root">
+        {/* <div className="mt-4 flow-root">
           <div className="-my-2">
             <div className="box-content py-2 px-2 relative overflow-x-auto xl:overflow-visible">
               <div className="grid justify-items-center grid-cols-2 md:grid-cols-3 gap-y-8 gap-x-8 sm:px-6 lg:px-8 xl:relative xl:px-0 xl:space-x-0 xl:gap-x-8">
-                {collections.map((collection) => (
+                {/* {collections.map((collection) => (
                   <CollectionCard key={collection.id} collection={collection} />
-                ))}
+                ))} *
               </div>
             </div>
           </div>
-        </div>
+        </div> 
 
         <div className="mt-6 px-4 sm:hidden">
           <a
             href="~/routes/__cart/index#"
             className="block text-sm font-semibold text-primary-600 hover:text-primary-500"
           >
-            {t('common.browseCategories')}
-            <span aria-hidden="true"> &rarr;</span>
+            {/* {t('common.browseCategories')} 
+            {/* <span aria-hidden="true"> &rarr;</span>
           </a>
         </div>
-      </section>
+      </section> */}
     </>
   );
 }
