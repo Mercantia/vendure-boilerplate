ca2a0d7cb9622f128e119d91651ceefe592ed379
Antonio Müller <mullerjk@gmail.com>
2024-10-23 22:50:27 -0300

    payments

--- vendure-storefront/app/routes/$seller/checkout/payment.tsx
+++ vendure-storefront/app/routes/$seller/checkout/payment.tsx
@@ -17,6 +17,116 @@ import { getActiveOrder } from '~/providers/orders/order';
 import { getSessionStorage } from '~/sessions';
 import { useTranslation } from 'react-i18next';
 
+export async function loader({ params, request }: DataFunctionArgs) {
+  const session = await getSessionStorage().then((sessionStorage) =>
+    sessionStorage.getSession(request?.headers.get('Cookie')),
+  );
+  const activeOrder = await getActiveOrder({ request });
+
+  // Obtenha o seller da URL
+  const seller = params.seller;
+
+  // Check if there is an active order if not redirect to homepage
+  if (
+    !session ||
+    !activeOrder ||
+    !activeOrder.active ||
+    activeOrder.lines.length === 0
+  ) {
+    return redirect('/');
+  }
+
+  const { eligiblePaymentMethods } = await getEligiblePaymentMethods({
+    request,
+  });
+  const error = session.get('activeOrderError');
+  let stripePaymentIntent: string | undefined;
+  let stripePublishableKey: string | undefined;
+  let stripeError: string | undefined;
+  if (eligiblePaymentMethods.find((method) => method.code.includes('stripe'))) {
+    try {
+      const stripePaymentIntentResult = await createStripePaymentIntent({
+        request,
+      });
+      stripePaymentIntent =
+        stripePaymentIntentResult.createStripePaymentIntent ?? undefined;
+      stripePublishableKey = process.env.STRIPE_PUBLISHABLE_KEY;
+    } catch (e: any) {
+      stripeError = e.message;
+    }
+  }
+
+  let brainTreeKey: string | undefined;
+  let brainTreeError: string | undefined;
+  if (
+    eligiblePaymentMethods.find((method) => method.code.includes('braintree'))
+  ) {
+    try {
+      const generateBrainTreeTokenResult = await generateBraintreeClientToken({
+        request,
+      });
+      brainTreeKey =
+        generateBrainTreeTokenResult.generateBraintreeClientToken ?? '';
+    } catch (e: any) {
+      brainTreeError = e.message;
+    }
+  }
+
+  return json({
+    eligiblePaymentMethods,
+    stripePaymentIntent,
+    stripePublishableKey,
+    stripeError,
+    brainTreeKey,
+    brainTreeError,
+    error,
+    seller, // Retornar o seller no loader
+  });
+}
+
+export async function action({ params, request }: DataFunctionArgs) {
+  const body = await request.formData();
+  const paymentMethodCode = body.get('paymentMethodCode');
+  const paymentNonce = body.get('paymentNonce');
+  
+  const seller = params.seller; // Obtenha o seller dos parâmetros
+  
+  if (typeof paymentMethodCode === 'string') {
+    const { nextOrderStates } = await getNextOrderStates({
+      request,
+    });
+    if (nextOrderStates.includes('ArrangingPayment')) {
+      const transitionResult = await transitionOrderToState(
+        'ArrangingPayment',
+        { request },
+      );
+      if (transitionResult.transitionOrderToState?.__typename !== 'Order') {
+        throw new Response('Not Found', {
+          status: 400,
+          statusText: transitionResult.transitionOrderToState?.message,
+        });
+      }
+    }
+
+    const result = await addPaymentToOrder(
+      { method: paymentMethodCode, metadata: { nonce: paymentNonce } },
+      { request },
+    );
+    if (result.addPaymentToOrder.__typename === 'Order') {
+      // Redirecionar para a página de confirmação com o seller na URL
+      return redirect(
+        `/${seller}/checkout/confirmation/${result.addPaymentToOrder.code}`,
+      );
+    } else {
+      throw new Response('Not Found', {
+        status: 400,
+        statusText: result.addPaymentToOrder?.message,
+      });
+    }
+  }
+}
+
+
 export default function CheckoutPayment() {
   const {
     eligiblePaymentMethods,
@@ -85,6 +195,7 @@ export default function CheckoutPayment() {
     </div>
   );
 }
+
 
 function getPaymentError(error?: ErrorResult): string | undefined {
   if (!error || !error.errorCode) {
